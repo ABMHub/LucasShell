b0c260b019c18116073777c8b49acdc9d495b8e2
ABMHub <lucasabmacedo@hotmail.com>
2022-06-20 14:41:42 -0300

    Parse de comandos estruturado

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <list>
+#include <sstream>
+#include <vector>
+
 using namespace std;
+
+typedef struct {
+  string msg;
+  int cod; // -1: exit, 0: deu ruim mas nao faca nada, 1: deu bom
+} ReturnFlag;
 
 class History {
   private:
@@ -39,7 +47,7 @@ class History {
 
     // Recupera string do comando da posicao especificada
     string get_command (int position) {
-      if (position < 0 || position > cmd_count) return "Este comando nao existe";
+      if (position < 0 || position > cmd_count) return "";
       position--; // agora a range eh 0 - 9, ao inves de 1 - 10
 
       auto it = hist.begin();
@@ -50,28 +58,55 @@ class History {
     }
 };
 
-// void function_switch(string user_input) {
+// Funcao que recebe uma string, e retorna um vetor de strings, divididas pelos espacos em branco.
+vector<string> string_split(string cmd) {
+  vector<string> ret;
+  stringstream stream(cmd);
+  string word;
 
-// }
+  while (stream >> word) {
+    ret.push_back(word);
+  }
+  return ret;
+}
+
+History hist;
+
+ReturnFlag function_switch(string user_input) {
+  vector<string> command_vec = string_split(user_input);
+  if (command_vec.size() == 0) return {"Comando nao identificado.", 0};
+
+  if (command_vec[0] == "historico") {
+    if (command_vec.size() > 2) return {"O comando historico recebe um ou nenhum parametros.", 0};
+    else if (command_vec.size() == 1) {
+      hist.print_hist();
+      return {"", 1};
+    }
+    else if (command_vec.size() == 2) {
+      int position = stoi(command_vec[1]);
+      string cmd = hist.get_command(position);
+      if (cmd == "") return {"Esta posicao no historico (ainda) nao existe", 0};
+      return function_switch(cmd); // ! possibilidade de loop infinito de comandos
+    }
+  }
+
+  if (command_vec[0] == "exit") {
+    return {"", -1};
+  }
+
+  return {"Comando nao identificado", 0};
+}
 
 int main () {
   bool exit = false;
-  History hist;
   while (!exit) {
     cout << "lucash> ";
     string user_input;
     getline(cin, user_input);
 
-    if (user_input == "historico") {
-      hist.print_hist();
-    }
-
-    // ! gambiarra para testes
-    if (user_input == "historico2") {
-      cout << hist.get_command(2) << endl;
-    }
-
-    else if (user_input == "exit") exit = true;
+    auto cmd_response = function_switch(user_input);
+    if (cmd_response.msg != "") cout << cmd_response.msg << endl;
+    if (cmd_response.cod == -1) exit = true;
     
     hist.create_elem(user_input);
   }
